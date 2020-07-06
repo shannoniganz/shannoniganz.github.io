1d0ab5b875420b1f697673b5cb149a9a2ed46268
Naeem Mannan <nmannan97@gmail.com>
2020-07-05 19:46:51 -0700

    Updates

---
+++ precache-manifest.b04d58eaeea209e10445b79f56d1ea42.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e347bff3abeba2419032455659e24950",
+    "url": "/shannoniganz.github.io/index.html"
+  },
+  {
+    "revision": "c29b803e778937889a27",
+    "url": "/shannoniganz.github.io/static/css/main.147df6aa.chunk.css"
+  },
+  {
+    "revision": "29cb56b2866ca2d04543",
+    "url": "/shannoniganz.github.io/static/js/2.23131a64.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/shannoniganz.github.io/static/js/2.23131a64.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c29b803e778937889a27",
+    "url": "/shannoniganz.github.io/static/js/main.fa948125.chunk.js"
+  },
+  {
+    "revision": "5516a5134bcc53c2afda",
+    "url": "/shannoniganz.github.io/static/js/runtime-main.0c8b7388.js"
+  },
+  {
+    "revision": "f03e2575eb23dbd46081b484ef00902f",
+    "url": "/shannoniganz.github.io/static/media/Amazing 2.f03e2575.PNG"
+  },
+  {
+    "revision": "701e3337fdc8074c71d2eb398472031f",
+    "url": "/shannoniganz.github.io/static/media/Amazing 3.701e3337.PNG"
+  },
+  {
+    "revision": "94859c142586398c4811a010a163d0db",
+    "url": "/shannoniganz.github.io/static/media/Amazing 4.94859c14.PNG"
+  },
+  {
+    "revision": "0e4fecd43dcdaa01685c88d325201f4a",
+    "url": "/shannoniganz.github.io/static/media/Amazing1.0e4fecd4.PNG"
+  },
+  {
+    "revision": "a88e4acda395e112a7284da7fe8d7324",
+    "url": "/shannoniganz.github.io/static/media/Capture.a88e4acd.PNG"
+  },
+  {
+    "revision": "796def1a8967820cbea19a1946cbd609",
+    "url": "/shannoniganz.github.io/static/media/MainaPic.796def1a.PNG"
+  }
+]);
